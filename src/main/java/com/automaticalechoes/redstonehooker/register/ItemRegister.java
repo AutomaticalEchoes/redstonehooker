6f8749c4973008d82e2a066174ec915cb2b24f40
1121665965 <1121665965@qq.com>
2023-10-12 18:29:49 +0800

    显示更新

--- src/main/java/com/automaticalechoes/redstonehooker/register/ItemRegister.java
+++ src/main/java/com/automaticalechoes/redstonehooker/register/ItemRegister.java
@@ -17,8 +17,8 @@ public class ItemRegister {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RedstoneHooker.MODID);
     public static final ArrayList<RegistryObject<? extends Item>> MOD_ITEMS = new ArrayList<>();
     public static final RegistryObject<BlockItem> CONTAINER_PROXY_BLOCK_ITEM = Register("container_proxy_block_item", () -> new BlockItem(BlockRegister.CONTAINER_PROXY_BLOCK.get(),new Item.Properties()));
-    public static final RegistryObject<BlockItem> ANISOTROPIC_SIGNAL_BLOCK_ITEM = Register("anisotropic_signal_block_item", () -> new BlockItem(BlockRegister.ANISOTROPIC_SIGNAL_BLOCK.get(),new Item.Properties()));
     public static final RegistryObject<BlockItem> INVENTORY_ENTITY_PROXY_BLOCK_ITEM = Register("inventory_entity_proxy_block_item", () -> new BlockItem(BlockRegister.INVENTORY_ENTITY_PROXY_BLOCK.get(),new Item.Properties()));
+    public static final RegistryObject<BlockItem> ANISOTROPIC_SIGNAL_BLOCK_ITEM = Register("anisotropic_signal_block_item", () -> new BlockItem(BlockRegister.ANISOTROPIC_SIGNAL_BLOCK.get(),new Item.Properties()));
     public static final RegistryObject<HookerAdjustItem> HOOKER_ADJUST = Register("hooker_adjust_item", () -> new HookerAdjustItem(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<AddressItem> ADDRESS_ITEM = Register("address_item",() -> new AddressItem(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<AddressNameTagItem> ADDRESS_NAME_TAG_ITEM = Register("address_name_tag_item",() -> new AddressNameTagItem(new Item.Properties().stacksTo(1)));
